9acee7c404b36dd9698c25ee73627dfd1bb7b9b7
roman-butsenko <r.r.butsenko@gmail.com>
2023-08-22 21:30:54 +0300

    transmission interruption not done fully

--- uart_out_2.sim/sim_1/behav/xsim/xsim.dir/uart_out_simulation_behav/obj/xsim_1.c
+++ uart_out_2.sim/sim_1/behav/xsim/xsim.dir/uart_out_simulation_behav/obj/xsim_1.c
@@ -56,31 +56,37 @@ typedef void (*funcp)(char *, char *);
 extern int main(int, char**);
 IKI_DLLESPEC extern void execute_2(char*, char *);
 IKI_DLLESPEC extern void execute_3(char*, char *);
-IKI_DLLESPEC extern void execute_18(char*, char *);
-IKI_DLLESPEC extern void execute_19(char*, char *);
+IKI_DLLESPEC extern void execute_28(char*, char *);
+IKI_DLLESPEC extern void execute_29(char*, char *);
 IKI_DLLESPEC extern void execute_8(char*, char *);
-IKI_DLLESPEC extern void execute_9(char*, char *);
-IKI_DLLESPEC extern void execute_10(char*, char *);
+IKI_DLLESPEC extern void execute_14(char*, char *);
+IKI_DLLESPEC extern void execute_15(char*, char *);
+IKI_DLLESPEC extern void execute_16(char*, char *);
 IKI_DLLESPEC extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
 IKI_DLLESPEC extern void execute_6(char*, char *);
 IKI_DLLESPEC extern void execute_7(char*, char *);
-IKI_DLLESPEC extern void execute_12(char*, char *);
-IKI_DLLESPEC extern void execute_13(char*, char *);
-IKI_DLLESPEC extern void execute_14(char*, char *);
-IKI_DLLESPEC extern void execute_15(char*, char *);
-IKI_DLLESPEC extern void execute_20(char*, char *);
-IKI_DLLESPEC extern void execute_21(char*, char *);
-IKI_DLLESPEC extern void execute_22(char*, char *);
-IKI_DLLESPEC extern void execute_23(char*, char *);
 IKI_DLLESPEC extern void execute_24(char*, char *);
 IKI_DLLESPEC extern void execute_25(char*, char *);
+IKI_DLLESPEC extern void execute_26(char*, char *);
+IKI_DLLESPEC extern void execute_27(char*, char *);
+IKI_DLLESPEC extern void execute_11(char*, char *);
+IKI_DLLESPEC extern void execute_18(char*, char *);
+IKI_DLLESPEC extern void execute_19(char*, char *);
+IKI_DLLESPEC extern void execute_20(char*, char *);
+IKI_DLLESPEC extern void execute_21(char*, char *);
+IKI_DLLESPEC extern void execute_30(char*, char *);
+IKI_DLLESPEC extern void execute_31(char*, char *);
+IKI_DLLESPEC extern void execute_32(char*, char *);
+IKI_DLLESPEC extern void execute_33(char*, char *);
+IKI_DLLESPEC extern void execute_34(char*, char *);
+IKI_DLLESPEC extern void execute_35(char*, char *);
 IKI_DLLESPEC extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
-funcp funcTab[21] = {(funcp)execute_2, (funcp)execute_3, (funcp)execute_18, (funcp)execute_19, (funcp)execute_8, (funcp)execute_9, (funcp)execute_10, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_6, (funcp)execute_7, (funcp)execute_12, (funcp)execute_13, (funcp)execute_14, (funcp)execute_15, (funcp)execute_20, (funcp)execute_21, (funcp)execute_22, (funcp)execute_23, (funcp)execute_24, (funcp)execute_25, (funcp)vlog_transfunc_eventcallback};
-const int NumRelocateId= 21;
+funcp funcTab[27] = {(funcp)execute_2, (funcp)execute_3, (funcp)execute_28, (funcp)execute_29, (funcp)execute_8, (funcp)execute_14, (funcp)execute_15, (funcp)execute_16, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_6, (funcp)execute_7, (funcp)execute_24, (funcp)execute_25, (funcp)execute_26, (funcp)execute_27, (funcp)execute_11, (funcp)execute_18, (funcp)execute_19, (funcp)execute_20, (funcp)execute_21, (funcp)execute_30, (funcp)execute_31, (funcp)execute_32, (funcp)execute_33, (funcp)execute_34, (funcp)execute_35, (funcp)vlog_transfunc_eventcallback};
+const int NumRelocateId= 27;
 
 void relocate(char *dp)
 {
-	iki_relocate(dp, "xsim.dir/uart_out_simulation_behav/xsim.reloc",  (void **)funcTab, 21);
+	iki_relocate(dp, "xsim.dir/uart_out_simulation_behav/xsim.reloc",  (void **)funcTab, 27);
 
 	/*Populate the transaction function pointer field in the whole net structure */
 }
--- uart_out_2.srcs/sources_1/new/uart_out_2.v
+++ uart_out_2.srcs/sources_1/new/uart_out_2.v
@@ -1,30 +1,24 @@
 `timescale 1ns / 10ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: hanging out co.
+// Engineer: Roman Butsenko
 // 
 // Create Date: 10.08.2023 20:51:59
 // Design Name: 
 // Module Name: uart_out
-// Project Name: 
-// Target Devices: 
+// Project Name: UART out
+// Target Devices: Basys 3
 // Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Description: communicating between terra term console and FPGA
+ 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module uart_out(
     input CLK100MHZ,
+    input button,
     output uart_tx,
     input uart_rx,
-    input button,
     output data_led
     );
     
@@ -32,14 +26,15 @@ module uart_out(
     reg uart_tx_reg;
     assign uart_tx = uart_tx_reg;
     
-    wire clk;
+    wire clk; //
     
+    //clk generator
     clk_gen clk_gen_instance(
         .CLK100MHZ(CLK100MHZ),
         .clk(clk)
         );
-        
-    reg [3:0] bit_count = 0;
+            
+    reg [3:0] bit_count = 0; //this register counts the bits in a transmitted byte
     
     `include "C:\Xilinx\projects\uart_out_basys_3\uart_out_2.srcs\sources_1\new\asci_characters.v"
     
@@ -47,12 +42,7 @@ module uart_out(
     start_bit = 1'b0,
     stop_bit = 1'b1;
     
-    // This is very important, in UART least significant bit
-    // goes first (right after the start bit), so in order for
-    // it to line up with the ASCII simbol instantiation, this
-    // register is created a bit wierd
-    
-    reg [9:0] bit = 0;
+    reg [9:0] byte = 0;
     
     // each following package needs the bit to be different,
     // so the bit is formed dinamycally 
@@ -60,42 +50,70 @@ module uart_out(
     // We're gonna write "Hello, world![new_line]", 14 characters in total
     
     reg [3:0] letter_count = 0; // this register has to store at least 14 states
+                                // is stores the number of the current letter in the transmission
+    
+    // This is very important, in UART least significant bit
+    // goes first (right after the start bit), so in order for
+    // it to line up with the ASCII simbol instantiation, this
+    // register is created a bit wierd
     
     always@(*) begin
         case(letter_count)
-            4'd0: bit = {stop_bit, H_let, start_bit};
-            4'd1: bit = {stop_bit, e_let, start_bit};
-            4'd2: bit = {stop_bit, l_let, start_bit};
-            4'd3: bit = {stop_bit, l_let, start_bit};
-            4'd4: bit = {stop_bit, o_let, start_bit};
-            4'd5:  bit = {stop_bit, comma_let, start_bit};
-            4'd6: bit = {stop_bit, space_let, start_bit};
-            4'd7: bit = {stop_bit, w_let, start_bit};
-            4'd8: bit = {stop_bit, o_let, start_bit};
-            4'd9: bit = {stop_bit, r_let, start_bit};
-            4'd10: bit = {stop_bit, l_let, start_bit};
-            4'd11: bit = {stop_bit, d_let, start_bit};
-            4'd12: bit = {stop_bit, exclam_let, start_bit};
-            4'd13: bit = {stop_bit, new_line_let, start_bit};
-            4'd14: bit = {stop_bit, return_let, start_bit};
-            default:  bit = {stop_bit, exclam_let, start_bit};
+            4'd0: byte = {stop_bit, H_let, start_bit};
+            4'd1: byte = {stop_bit, e_let, start_bit};
+            4'd2: byte = {stop_bit, l_let, start_bit};
+            4'd3: byte = {stop_bit, l_let, start_bit};
+            4'd4: byte = {stop_bit, o_let, start_bit};
+            4'd5: byte = {stop_bit, comma_let, start_bit};
+            4'd6: byte = {stop_bit, space_let, start_bit};
+            4'd7: byte = {stop_bit, w_let, start_bit};
+            4'd8: byte = {stop_bit, o_let, start_bit};
+            4'd9: byte = {stop_bit, r_let, start_bit};
+            4'd10: byte = {stop_bit, l_let, start_bit};
+            4'd11: byte = {stop_bit, d_let, start_bit};
+            4'd12: byte = {stop_bit, exclam_let, start_bit};
+            4'd13: byte = {stop_bit, new_line_let, start_bit};
+            4'd14: byte = {stop_bit, return_let, start_bit};
+            default:  byte = {stop_bit, x_let, start_bit};
         endcase
-    end 
+    end
     
+    // logic, that describes the signal to start the transmission
+    reg transmit = 0;
+    wire button_deb;
+    
+    debounce button_debiunce(
+        .sig(button),
+        .clk(clk),
+        .sig_debounced(button_deb)
+        );
+        
+    always@(*) begin 
+    if (button_deb) transmit <= 1;
+    if (letter_count == 4'd14) transmit <= 0;
+    
+    //TX line control
     always @ (posedge clk) begin
-        if (!button) begin
+        if (!transmit) begin
             uart_tx_reg <= 1;
             bit_count = 0;
         end
-        else begin
-            uart_tx_reg <= bit[bit_count];
+        else if (transmit) begin
+            uart_tx_reg <= byte[bit_count];
             if (bit_count == 9) begin
                 bit_count <= 0;
-                letter_count <= (letter_count == 4'd14) ? 4'd0 : letter_count + 1;
+                
+                if (letter_count == 4'd14) begin
+                    letter_count = 4'd0;            // transmission is finished until
+                                     // the button is pressed again (see button_debounce)
+                end
+                else letter_count = letter_count + 1; // switching to the next symbol
             end
             else bit_count <= bit_count + 1;          
         end
-    end 
+    end
+    
+    
     
     
 endmodule
@@ -124,6 +142,36 @@ module clk_gen(
     
 endmodule
 
+//Debouncing module
+// if input is high, output will go high for 1 clk period
+// without that module transmission is
+// interrupted if the butten is let go off
+module debounce(
+    input sig,
+    input clk,
+    output sig_debounced
+    );
+    
+    wire dff_1_out;
+    wire dff_2_out;
+    wire inv_dff_2_out;
+    
+    dff dff_deb_1 (.clk(clk), .d(sig), .q(dff_1_out));
+    dff dff_deb_2 (.clk(clk), .d(dff_1_out), .q(dff_2_out));
+    
+    assign inv_dff_2_out = ~dff_2_out;
+    
+    assign sig_debounced = inv_dff_2_out & dff_1_out;
+    
+endmodule
 
-
-// 
+// just a simple D-flipflop
+module dff(
+    input clk,
+    input d,
+    output reg q
+    );
+    
+    always@ (posedge clk) q<=d;
+    
+endmodule
